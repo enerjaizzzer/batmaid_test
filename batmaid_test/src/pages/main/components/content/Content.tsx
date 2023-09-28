fbd640243fe1408d67ff80bbfff2563d64d70245
aliakhovets <aliakhovets@aristeksystems.com>
2023-09-28 17:35:31 +0300

    feat: add project structure

--- batmaid_test/src/pages/main/components/content/Content.tsx
+++ batmaid_test/src/pages/main/components/content/Content.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import * as C from "../../../../components";
+import "./duck/styles.scss";
 
 const Content = () => {
-  return <div className="content-container">123</div>;
+  return (
+    <div className={"content-container"}>
+      <C.Radio />
+      <C.Table />
+    </div>
+  );
 };
 
 export default Content;
